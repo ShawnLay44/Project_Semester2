d5ff970d74070e6bfd2f1af988b1a23a7b7fb517
ShawnLay44 <kobe13678010522@163.com>
2025-03-17 14:49:56 +0800

    Add exercise4.py to repository

--- exercise4.py
+++ exercise4.py
@@ -1,7 +1,14 @@
+#Author:Wulei
+#DATE:2025/3/14
+#Enter your age to check if any discount you are qualified for
+#Enter your age
 a = input("Enter your age:")
 if int(a) <= 19:
+#the situation under 19
 	print("you are qualified for student discounts!")
 elif  55 >int(a) >19:
+#the situation between 19 and 55
 	print("you are qualified for no age discounts")
 else:
+#other situation
 	print("you are qualified for senior discounts!")
